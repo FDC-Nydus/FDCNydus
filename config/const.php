1f68b6ded95324137b659b4a096b7bbc6a1019e3
Lester Ag Padul <padulleste@gmail.com>
2015-10-06 14:36:51 +0800

    fdc-nydus [update]

--- config/const.php
+++ config/const.php
@@ -5,4 +5,5 @@ CONST SH_PULL = "pull.sh";
 CONST SH_CONFLICTS = "conflict.sh";
 CONST SH_CLEAR_CONFLICT = "abortMerge.sh";
 CONST GIT_BRANCH = "master";
-CONST GIT_BRANCH_LABEL = "Master Branch";
+CONST GIT_BRANCH_LABEL = "Development Branch";
+CONST SITE_NAME = "http//english.fdc-inc.com";
--- inc/FDCWebhook.php
+++ inc/FDCWebhook.php
@@ -32,14 +32,9 @@ class FDCWebhook{
 		$return['pull_result'] = $this->executeCommand('cd /vagrant/workspace/FDCDevRepo && git reset --hard FETCH_HEAD 2>&1');
 		$this->executeCommand('cd /vagrant/workspace/FDCDevRepo && git clean -df');
 
-		// get list of files to be pushed
-		$return['file_list'] = $this->executeCommand('cd /vagrant/workspace/FDCDevRepo && git diff --stat');
-
 		// return for hook window
 		echo "PULL RETURN \n";
-		echo $return['pull_result']. "\n";
-		echo "CHANGED FILES \n";
-		echo $return['file_list']. "\n";
+		echo $return['pull_result'];
 
 		// handle the git result
 		$this->handleGitResult($return);
@@ -47,40 +42,31 @@ class FDCWebhook{
 
 	// handle the git result
 	public function handleGitResult($result){
-		// check if conflicts occured or some other error occured.
-		// in the result string, for "pull", status_code=0 = success, status_code=1 = fail, status_code=<anything else> = we'll assume as fail
-		// if errors occured
-		// execute abortMerge.sh in sh_commands to return the branch to its state before the merge
-		
 		// contains the message for slack
 		$slackMessage = "";
-
-		/*// if something went wrong, abort merge
-		if (strpos($result, "status_code=0") === FALSE) {
-			// abort merging
-			$abort = $this->executeCommand('sh ' . dirname(DIR) . '/' . SH_DIR . '/' . SH_CLEAR_CONFLICT);
-			
-			$errData = array(
-					'subject' => 'Merge Error',
-					'content' => $result
-			);
-			//$this->notification->writeError($errData);
-			// return for hook window
-			echo "ABORT MESSAGE \n";
-			echo $abort . "\n";
-		}*/
-
-		// slack message construction
+		$slackMessage .= "*PULL RESULT*";
 		$slackMessage .= "```";
-		$slackMessage .= "Date: ".date('Y/m/d H:i:s');
-		$slackMessage .= "LIST OF FILES CHANGED";
 		$slackMessage .= $result;
+		$slackMessage .= "```\n";
+
+		$slackMessage .= "*COMMIT LINK*";
+		$slackMessage .= "```";
+		$slackMessage .= "";
+		$slackMessage .= "```";
+
+		$slackMessage .= "*SITE*";
+		$slackMessage .= "```";
+		$slackMessage .= SITE_NAME;
 		$slackMessage .= "```";
 
 		// slack username
-		$slackUname = GIT_BRANCH_LABEL." auto deployment ".date("Y-m-d H:i:s");
+		$slackUname = GIT_BRANCH_LABEL." Auto Deployment " . date('Y/m/d H:i:s');
 
-			self::$slack->sendSlack($slackMessage);
+		// set the slack username
+		self::$slack->username = $slackUname;
+
+		// send slack message
+		self::$slack->sendSlack($slackMessage);
 	}
 
 	// execute command
